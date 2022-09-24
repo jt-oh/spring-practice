dfdc161d93acf4298f1625979dfd0b39f28dbddd
JeongTaek Oh <tei1234tei@gmail.com>
2022-09-25 00:59:28 +0900

    add web-content serving endpoint

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,6 +13,13 @@ public class GreetingController {
 
     private static final String TEMPLATE = "Hello, %s!";
     private static AtomicLong counter = new AtomicLong();
+
+    @GetMapping("greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "greeting";
+    }
 
     @GetMapping("api/v1/greeting")
     @ResponseBody
