9c371877bbb17c13201dfe7080b8780c74dc98eb
JeongTaek Oh <tei1234tei@gmail.com>
2022-09-25 00:57:05 +0900

    change api endpoint and rest controller annotations

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -4,15 +4,17 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class GreetingController {
 
     private static final String TEMPLATE = "Hello, %s!";
     private static AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
+    @GetMapping("api/v1/greeting")
+    @ResponseBody
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
