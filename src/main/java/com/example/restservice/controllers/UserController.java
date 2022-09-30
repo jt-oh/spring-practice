6c4d5ae24d6202a3059297dec842459b73f07a19
JeongTaek Oh <tei1234tei@gmail.com>
2022-10-01 04:12:52 +0900

    add user show API

--- src/main/java/com/example/restservice/controllers/UserController.java
+++ src/main/java/com/example/restservice/controllers/UserController.java
@@ -3,26 +3,35 @@ package com.example.restservice.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.restservice.controllers.responses.UserResponse;
 import com.example.restservice.domains.User.User;
 import com.example.restservice.repositories.UserRepository.UserRepository;
 
 @RestController
 @RequestMapping(path="/api/v1/users")
 public class UserController {
-  @Autowired
-  private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-  @PostMapping(path="")
+    @PostMapping(path="")
     public Integer addNewUser (@RequestParam String name, @RequestParam String email) {
         User user = new User(name, email);
-
+        
         User createdUser = userRepository.save(user);
-    
+        
         return createdUser.getId();
-  }
+    }
 
-  @GetMapping(path="")
-  public Iterable<User> getAllUsers() {
-    return userRepository.findAll();
-  }
+    @GetMapping(path="")
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping(path="/{user_id}")
+    public UserResponse getUser(@PathVariable("user_id") Integer userId) {
+        User user = userRepository.findById(userId)
+            .orElseThrow();
+
+        return new UserResponse(user);
+    }
 }
