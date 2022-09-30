19322d4a0adf80805cf7bc17b42a391b27916909
JeongTaek Oh <tei1234tei@gmail.com>
2022-10-01 04:07:35 +0900

    compress annotations

--- src/main/java/com/example/restservice/controllers/UserController.java
+++ src/main/java/com/example/restservice/controllers/UserController.java
@@ -1,17 +1,12 @@
 package com.example.restservice.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.restservice.domains.User.User;
 import com.example.restservice.repositories.UserRepository.UserRepository;
 
-@Controller
+@RestController
 @RequestMapping(path="/api/v1/users")
 public class UserController {
   @Autowired
@@ -30,7 +25,6 @@ public class UserController {
   }
 
   @GetMapping(path="")
-  @ResponseBody
   public Iterable<User> getAllUsers() {
     return userRepository.findAll();
   }
