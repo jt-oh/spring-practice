cfaa4ab344eba6578d59eb02d1baafc29fc4c082
JeongTaek Oh <tei1234tei@gmail.com>
2022-10-01 04:03:24 +0900

    use lombok annotations for getter and setter

--- src/main/java/com/example/restservice/domains/User/User.java
+++ src/main/java/com/example/restservice/domains/User/User.java
@@ -5,6 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class User {
     @Id
@@ -15,27 +22,8 @@ public class User {
 
     private String email;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
+    public User(String name, String email) {
         this.name = name;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-    
-    public void setEmail(String email) {
         this.email = email;
     }
 }
